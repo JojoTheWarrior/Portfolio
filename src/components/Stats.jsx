fc595ac5a88aad73094fb0d89ff77188827c125a
JojoTheWarrior <blahblahblah123toronto@gmail.com>
2024-08-01 08:58:23 -0400

    duolingo card

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 import axios from 'axios';
 import duolingo from '../assets/images/duolingo.jpg';
 import dmoj from '../assets/images/dmoj.jpg';
-import 
+import duo_flame from '../assets/images/flame.svg';
 
 const Stats = () => {
     const [streak, setStreak] = useState(-1)
@@ -27,13 +27,14 @@ const Stats = () => {
         </div>
         {/* Cards */}
         <div className="flex justify-evenly items-center">
-            <div className="background-white rounded-lg">
+            <div className="bg-white rounded-xl p-4vh" style={{width: "500px;", height: "250px;"}}>
                 <div className="flex"> 
-                    <img className="rounded-md" src={duolingo}></img>
-                    <div className="text-4xl flex font-black">
-                        <img src={flame}></img>
-                        <p>Day Streak<p/>
-                        <p>Total XP</p>
+                    <img className="rounded-xl h-48 w-48" src={duolingo}></img>
+                    <div className="text-4xl font-ubuntu flex-col font-black">
+                        <div className="flex align-center justify-center items-center">
+                            <img className="h-16 w-16" src={duo_flame}/>
+                            <p>{streak == -1 ? "Loading " : `${streak} Day `} Streak{streak == -1 ? "..." : ""}</p>
+                        </div>
                     </div>
                 </div>
             </div>
