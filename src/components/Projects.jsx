1e5d8cf1584d5e6a3975315339b8aef8791b3a7e
JojoTheWarrior <blahblahblah123toronto@gmail.com>
2024-08-08 08:39:12 -0400

    added project card

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -7,15 +7,13 @@ const Projects = () => {
         <div>
             {/* Switch */}
             <div className="flex justify-center w-full mt-25vh font-ubuntu font-bold text-white text-2xl">
-                <button className={`p-2 ${view=="grid" ? `w-60vw` : 'w-20vw'} bg-gradient-to-l from-button-lighter-blue to-button-light-blue
-                    ${view=="categorical" ? "border-4 border-button-dark-blue bg-opacity-50" : ""}
-                    transition-all duration-300 ease-in-out`}
+                <button className={`rounded-l-lg p-2 ${view=="grid" ? `w-60vw` : 'w-20vw'} bg-gradient-to-r from-button-lighter-blue to-button-light-blue
+                    transition-all duration-700 ease-in-out`}
                     onClick={() => setView("grid")}>
                     Grid View
                 </button>
-                <button className={`p-2 ${view=="categorical" ? `w-60vw` : 'w-20vw'} bg-gradient-to-r from-button-dark-blue to-button-darker-blue
-                    ${view=="grid" ? "border-4 border-button-lighter-blue bg-opacity-50" : ""}
-                    transition-all duration-300 ease-in-out`}
+                <button className={`rounded-r-lg p-2 ${view=="categorical" ? `w-60vw` : 'w-20vw'} bg-gradient-to-r from-button-dark-blue to-button-darker-blue
+                    transition-all duration-700 ease-in-out`}
                     onClick={() => setView("categorical")}>
                     Categorical View
                 </button>
