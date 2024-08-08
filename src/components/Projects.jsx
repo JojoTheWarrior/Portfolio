d462cd468fb9db85869b398b385cc1d237069a34
JojoTheWarrior <blahblahblah123toronto@gmail.com>
2024-08-08 08:28:30 -0400

    custom colors for buttons

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,18 +1,23 @@
 import React, { useState } from 'react'
 
 const Projects = () => {
-    
+    const [view, setView] = useState("grid");
 
     return (
         <div>
             {/* Switch */}
-            <div className="flex w-full mt-25vh font-ubuntu font-bold text-white">
-                <div className="w-40vw">
+            <div className="flex justify-center w-full mt-25vh font-ubuntu font-bold text-white text-2xl">
+                <button className={`p-2 w-40vw bg-gradient-to-l 
+                    ${view=="grid" ? "from-button-lighter-blue to-button-light-blue" : "from-button-dark-blue to-button-darker-blue"}
+                    ${view=="categorical" ? "border-4 border-button-dark-blue" : ""}`}
+                    onClick={}>
                     Grid View
-                </div>
-                <div className="w-40vw">
+                </button>
+                <button className={`p-2 w-40vw bg-gradient-to-r 
+                    ${view=="categorical" ? "from-button-lighter-blue to-button-light-blue" : "from-button-dark-blue to-button-darker-blue"}
+                    ${view=="grid" ? "border-4 border-button-lighter-blue" : ""}`}>
                     Categorical View
-                </div>
+                </button>
             </div>
 
         </div>
