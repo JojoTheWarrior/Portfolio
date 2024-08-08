f7c248eaf63d07a6b0bd8f7c2fc1618253799800
JojoTheWarrior <blahblahblah123toronto@gmail.com>
2024-08-08 08:34:14 -0400

    transitions for grid / categorical

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -7,15 +7,16 @@ const Projects = () => {
         <div>
             {/* Switch */}
             <div className="flex justify-center w-full mt-25vh font-ubuntu font-bold text-white text-2xl">
-                <button className={`p-2 w-40vw bg-gradient-to-l 
-                    ${view=="grid" ? "from-button-lighter-blue to-button-light-blue" : "from-button-dark-blue to-button-darker-blue"}
-                    ${view=="categorical" ? "border-4 border-button-dark-blue" : ""}`}
-                    onClick={}>
+                <button className={`p-2 ${view=="grid" ? `w-60vw` : 'w-20vw'} bg-gradient-to-l from-button-lighter-blue to-button-light-blue
+                    ${view=="categorical" ? "border-4 border-button-dark-blue bg-opacity-50" : ""}
+                    transition-all duration-300 ease-in-out`}
+                    onClick={() => setView("grid")}>
                     Grid View
                 </button>
-                <button className={`p-2 w-40vw bg-gradient-to-r 
-                    ${view=="categorical" ? "from-button-lighter-blue to-button-light-blue" : "from-button-dark-blue to-button-darker-blue"}
-                    ${view=="grid" ? "border-4 border-button-lighter-blue" : ""}`}>
+                <button className={`p-2 ${view=="categorical" ? `w-60vw` : 'w-20vw'} bg-gradient-to-r from-button-dark-blue to-button-darker-blue
+                    ${view=="grid" ? "border-4 border-button-lighter-blue bg-opacity-50" : ""}
+                    transition-all duration-300 ease-in-out`}
+                    onClick={() => setView("categorical")}>
                     Categorical View
                 </button>
             </div>
