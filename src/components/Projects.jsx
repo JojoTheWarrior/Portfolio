231e8b583baca47e53a624fd63aafe2f4b0a2f4c
JojoTheWarrior <blahblahblah123toronto@gmail.com>
2024-08-08 21:41:10 -0400

    font files

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -12,17 +12,20 @@ const Projects = () => {
     return (
         <div>
             {/* Switch */}
-            <div className="flex justify-center w-full mt-25vh font-ubuntu font-bold text-white text-2xl">
-                <button className={`rounded-l-lg p-2 ${view=="grid" ? `w-60vw` : 'w-20vw'} bg-gradient-to-r from-button-lighter-blue to-button-light-blue
-                    transition-all duration-700 ease-in-out`}
-                    onClick={() => setView("grid")}>
-                    Grid View
-                </button>
-                <button className={`rounded-r-lg p-2 ${view=="categorical" ? `w-60vw` : 'w-20vw'} bg-gradient-to-r from-button-dark-blue to-button-darker-blue
-                    transition-all duration-700 ease-in-out`}
-                    onClick={() => setView("categorical")}>
-                    Categorical View
-                </button>
+            <div className="flex w-full justify-center">
+                <div className="flex justify-center w-80vw rounded-xl mt-25vh font-ubuntu font-bold text-white text-2xl
+                                shadow-glow">
+                    <button className={`rounded-l-lg p-2 ${view=="grid" ? `w-60vw` : 'w-20vw'} bg-gradient-to-r from-button-lighter-blue to-button-light-blue
+                        transition-all duration-700 ease-in-out`}
+                        onClick={() => setView("grid")}>
+                        Grid View
+                    </button>
+                    <button className={`rounded-r-lg p-2 ${view=="categorical" ? `w-60vw` : 'w-20vw'} bg-gradient-to-r from-button-dark-blue to-button-darker-blue
+                        transition-all duration-700 ease-in-out`}
+                        onClick={() => setView("categorical")}>
+                        Categorical View
+                    </button>
+                </div>
             </div>
             {   
                 view=="grid" ? 
