ffc679ad93daa142983ce1160c6cb951146e52ee
JojoTheWarrior <blahblahblah123toronto@gmail.com>
2024-08-17 11:33:56 -0400

    flame animation title

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import React from 'react';
 
 const HomePage = () => {
-  return (
-    <div>HomePage</div>
+    return (
+    <div>
+        <link rel="style" src="../assets/styles/HomePage.css"></link>
+        <div className="fire">
+            <h1 className="burn">Joshua</h1>
+            <hr></hr>
+            <h1 className="burn">Wang</h1>
+        </div>
+    </div>
   )
 }
 
