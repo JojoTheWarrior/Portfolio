fdc4f3de7ef643ab3211708435173c6c527c5cc6
JojoTheWarrior <blahblahblah123toronto@gmail.com>
2024-08-17 15:42:46 -0400

    removed fire

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 const HomePage = () => {
     return (
     <div>
-        <link rel="style" src="../assets/styles/HomePage.css"></link>
         <div className="fire">
             <h1 className="burn">Joshua</h1>
             <hr></hr>
