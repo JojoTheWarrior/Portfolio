91c38de702a5a279a95790bc2e647d3da19a53fc
JojoTheWarrior <blahblahblah123toronto@gmail.com>
2025-10-23 16:52:18 -0400

    updated about me for first-year at waterloo

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -9,8 +9,8 @@ const AboutMe = () => {
         <div className='font-ubuntu flex background-blur-sm mx-8vh p-1vh justify-evenly items-center'>
             <div className="text-white p-1vh backdrop-blur-lg">
               <h1 className="text-4xl">Hi, I'm <b>Joshua</b>!</h1>
-              <p className="text-2xl">I enjoy <b className="text-palette-lapis">web</b> + <b className="text-palette-lapis"> game development</b>, and <b className="text-palette-lapis">music</b></p>
-              <p className="text-2xl">I'm a Grade 12 student at William Lyon Mackenzie CI.</p>
+              <p className="text-2xl">I enjoy <b className="text-palette-lapis">quant finance</b>, <b className="text-palette-lapis">software development</b>, and <b className="text-palette-lapis">mathematics.</b></p>
+              <p className="text-2xl">I'm a first-year <b className="text-palette-lapis">Computer Science</b> student at the University of Waterloo.</p>
             </div>
             <div className="">
               <img src={temp_pfp} className="h-30vh"/>
